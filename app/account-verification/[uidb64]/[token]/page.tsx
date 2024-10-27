aa5fd67951f115ee60c6c9b5265051f28380d22e
owolabi-develop <owolabidevelop84@gmail.com>
2024-10-26 22:31:18 -0800

    update login api route

--- app/account-verification/[uidb64]/[token]/page.tsx
+++ app/account-verification/[uidb64]/[token]/page.tsx
@@ -13,7 +13,7 @@ const VerificationPage: React.FC<VerificationProps> = ({ params: { uidb64, token
   useEffect(() => {
     const verifyAccount = async () => {
       try {
-        const response = await fetch(`http://127.0.0.1:8000/careerportal/verify-email/${uidb64}/${token}/`, {
+        const response = await fetch(`https://career-app-vopqo.ondigitalocean.app/careerportal/verify-email/${uidb64}/${token}/`, {
           method: 'GET',
           headers: { 'Content-Type': 'application/json' },
         });

7a910d35e0648a57632643c1204a58384b86e7ec
owolabi-develop <owolabidevelop84@gmail.com>
2024-11-13 18:45:44 +0100

    added profile view count

--- app/ui/chart/applicantprofilechart.tsx
+++ app/ui/chart/applicantprofilechart.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import useSWR from 'swr';
+import Cookies from "js-cookie";
+import { useParams } from 'next/navigation'
 import { Line } from "react-chartjs-2";
 import {
   Chart as ChartJS,
@@ -14,23 +17,51 @@ import {
 } from "chart.js";
 
 ChartJS.register(LineElement, CategoryScale,Filler, LinearScale, PointElement, Tooltip, Legend);
+ 
 
-// Hardcoded data for the last 7 days
-const demoData = [
-  { date: "2024-11-01", views: 120 },
-  { date: "2024-11-02", views: 150 },
-  { date: "2024-11-03", views: 180 },
-  { date: "2024-11-04", views: 220 },
-  { date: "2024-11-05", views: 170 },
-  { date: "2024-11-06", views: 210 },
-  { date: "2024-11-07", views: 240 }
-];
+
+
+const ApplicantProfileViewChart: React.FC = () => {
+
+  type profileviewSchema ={
+    id:string,
+    date_viewed:string,
+    view_count:number
+  }
+  const params = useParams()
+  
+//  fetch profile views
+  const fetcher = (url: string) =>
+    fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${Cookies.get("item")}`,
+      },
+    }).then((r) => r.json());
+const { data:profileviews,error,isLoading} =   useSWR(`${process.env.NEXT_PUBLIC_API_BASE_URL}careerportal/get-applicant-profile-views/${params.id}/`, fetcher)
+console.log("new profile:",profileviews)
+
+if(isLoading){
+    return <div className='bg-slate-50 drop-shadow-lg rounded-md animate-pulse py-1 px-4'>
+    <div className='w-full bg-slate-300 py-1 rounded-full my-3'></div>
+    <div className='w-full bg-slate-300 py-1 rounded-full my-3'></div>
+    <div className='w-full bg-slate-300 py-1 rounded-full my-3'></div>
+    <div className='w-full bg-slate-300 py-1 rounded-full my-3'></div>
+    <div className='w-full bg-slate-300 py-1 rounded-full my-3'></div>
+    <div className='w-full bg-slate-300 py-1 rounded-full my-3'></div>
+    <div className='w-full bg-slate-300 py-1 rounded-full my-3'></div>
+</div>
+}
+if(error){
+    return <div>fail to fetch view data</div>
+}
+
 
 // Prepare chart data directly
-const labels = demoData.map((item) =>
-  new Date(item.date).toLocaleDateString("en-US", { month: "short", day: "numeric" })
+const labels = profileviews.map((item:profileviewSchema) =>
+  new Date(item.date_viewed).toLocaleDateString("en-US", { month: "short", day: "numeric" })
 );
-const data = demoData.map((item) => item.views);
+const data = profileviews.map((item:profileviewSchema) => item.view_count);
 
 const chartData: ChartData<"line"> = {
   labels,
@@ -82,8 +113,6 @@ const options: ChartOptions<"line"> = {
     },
   },
 };
-
-const ApplicantProfileViewChart: React.FC = () => {
   return (
      <div style={{ width: "100%", height: "50%" }}>
       <Line data={chartData} options={options} />

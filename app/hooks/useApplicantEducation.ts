14ba5fbb1016cda6cb238748c922c27300dcddc3
olawalle <olawalle94@gmail.com>
2024-11-22 16:05:21 +0100

    update biodata

---
+++ app/hooks/useApplicantEducation.ts
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import { cultureLancerAxios } from "../ui-services/axios";
+
+export const useApplicantEducation = () => {
+  const queryKey = "applicantEducation";
+
+  const GetApplicantEducation = async () => {
+    try {
+      const response = await cultureLancerAxios.get(`/applicant-education/`);
+      return response.data;
+    } catch (error) {
+      return {};
+    }
+  };
+
+  const query = useQuery({
+    queryKey: [queryKey],
+    queryFn: GetApplicantEducation,
+  });
+  return query;
+};
